1de9a1d0a47ec7dba6d8416413d85f8e30fd22b3
Tien <tienlindev@gmail.com>
2019-09-26 23:22:24 +1000

    Week 7 Ex 2.1

--- cis194/week7/tien/src/JoinList.hs
+++ cis194/week7/tien/src/JoinList.hs
@@ -1,6 +1,7 @@
+{-# OPTIONS_GHC -Wall #-}
 module JoinList where
 
-import Data.Monoid
+import Sized
 
 data JoinList m a
   = Empty
@@ -13,5 +14,30 @@ tag Empty = mempty
 tag (Single m _) = m
 tag (Append m jl1 jl2) = (tag jl1) <> (tag jl2)
 
+-- exercise 1 --
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) jl1 jl2 = Append (tag jl1 <> tag jl2) jl1 jl2
+
+-- exercise 2 --
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _          = Nothing
+_      !!? i | i < 0  = Nothing
+(x:xs) !!? 0 = Just x
+(_:xs) !!? i = xs !!? (i-1)
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty            = []
+jlToList (Single _ a)     = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
+indexJ' :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ' 1 (Single _ a) = Just a
+indexJ' x (Append _ left right)
+  | x <= (getSize . size . tag $ left) = indexJ' x left
+  | otherwise = indexJ' (x - (getSize . size . tag $ left)) right
+indexJ' _ _ = Nothing
+
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ = indexJ' . (+1)
+
--- cis194/week7/tien/test/JoinListSpec.hs
+++ cis194/week7/tien/test/JoinListSpec.hs
@@ -1,7 +1,8 @@
 import Data.Monoid
 import Test.Hspec
 
-import JoinList
+import  JoinList
+import Sized
 
 main :: IO ()
 main =
@@ -9,7 +10,17 @@ main =
     describe "JoinList" $ do
       describe "exercise 1" $ do
         it "should return correct JoinList" $ do
+          let jl1 = Single (Product 5) 'e'
+          let jl2 =
+                Append
+                  (Product 30)
+                  (Single (Product 10) 'g')
+                  (Single (Product 3) 'h')
           jl1 +++ jl2 `shouldBe` (Append (Product 150) jl1 jl2)
-  where
-    jl1 = Single (Product 5) 'e'
-    jl2 = Append (Product 30) (Single (Product 10) 'g') (Single (Product 3) 'h')
+
+      describe "exercise 2" $ do
+        it "should return Nothing if the index not found" $ do
+          let jl1 = Append (Size 4) (Append (Size 3) (Single (Size 1) 'a') (Append (Size 2) (Single (Size 1) 'b') (Single (Size 1) 'c'))) (Single (Size 1) 'd')
+          let jl2 = Append (Size 2) (Single (Size 1) 'e') (Single (Size 1) 'f')
+          let jl3 = Append (Size 6) jl1 jl2
+          indexJ 3 jl3 `shouldBe` (jlToList jl3 !!? 3)
