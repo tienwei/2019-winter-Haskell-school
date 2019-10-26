e33a7e10e7f2032f42597040a5c44b814409ca83
Tien Lin <tienlindev@gmail.com>
2019-10-26 13:02:50 +1100

    Add ex3 tests

--- cis194/week11/tien/test/SExprSpec.hs
+++ cis194/week11/tien/test/SExprSpec.hs
@@ -26,3 +26,16 @@ main =
           runParser ident "foo33fA" `shouldBe` Just ("foo33fA", "")
           runParser ident "2bad" `shouldBe` Nothing
           runParser ident "" `shouldBe` Nothing
+      describe "exercise 3" $ do
+        it "should return correct results of Parser SExpr" $ do
+          (show $ runParser parseSExpr "#") `shouldBe` "Nothing"
+          (show $ runParser parseSExpr "5") `shouldBe` "Just (A (N 5),\"\")"
+          (show $ runParser parseSExpr "foo3") `shouldBe`
+            "Just (A (I \"foo3\"),\"\")"
+          (show $ runParser parseSExpr "(bar (foo) 3 5 874)") `shouldBe`
+            "Just (Comb [A (I \"bar\"),Comb [A (I \"foo\")],A (N 3),A (N 5),A (N 874)],\"\")"
+          (show $
+           runParser parseSExpr "(((lambda x (lambda y (plus x y))) 3) 5)") `shouldBe`
+            "Just (Comb [Comb [Comb [A (I \"lambda\"),A (I \"x\"),Comb [A (I \"lambda\"),A (I \"y\"),Comb [A (I \"plus\"),A (I \"x\"),A (I \"y\")]]],A (N 3)],A (N 5)],\"\")"
+          (show $ runParser parseSExpr "( lots of ( spaces in ) this ( one ) )") `shouldBe`
+            "Just (Comb [A (I \"lots\"),A (I \"of\"),Comb [A (I \"spaces\"),A (I \"in\")],A (I \"this\"),Comb [A (I \"one\")]],\"\")"
